676b6a961cc9c4c7f763835d3cbf373861b3c507
artemkhodzhayan <a.khojayan@cyrm.ru>
2022-01-17 14:11:39 +0500

    RUP-49, added uniq id generator

--- src/components/Tabs/TabsHeader/TabsHeader.tsx
+++ src/components/Tabs/TabsHeader/TabsHeader.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import uniqid from 'uniqid';
 import { TabHeaderItem } from '../TabHeaderItem/TabHeaderItem';
 import { TabsHeaderContainer } from '../Tabs.styles';
 import { ITabs } from '../Tabs.types';
@@ -6,10 +7,7 @@ import { ITabs } from '../Tabs.types';
 const TabsHeader: FC<ITabs> = ({ tabs, activeIndex, onTabClick }) => (
   <TabsHeaderContainer>
     {tabs.map((tab, index) => (
-      <div className="tabs-header__header-item-container" key={tab.text}>
-        {/* TO-DO Так как ESlint ругается на использование индекса массива для key,
-            на данный момент используется название таба, но нужно установить
-            пакет для генерации уникадльных id и использовать их */}
+      <div className="tabs-header__header-item-container" key={uniqid()}>
         <TabHeaderItem
           text={tab.text}
           onClick={() => onTabClick(index)}
